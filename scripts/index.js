ece10f319aee1d6f22cc97df8e4e270423cf223c
nwctrinity <severmay123@mail.ru>
2025-02-15 03:33:39 +0500

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,30 @@
 
 // @todo: DOM узлы
 
+const placesList = document.querySelector('.places__list');
+
 // @todo: Функция создания карточки
+
+function addCard(initialCard, removeCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = initialCard.name;
+    cardElement.querySelector('.card__image').src = initialCard.link;
+    cardElement.querySelector('.card__image').alt = initialCard.name;
+    
+    placesList.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
 
+placesList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('card__delete-button')) {
+        const cardToRemove = event.target.closest('.card');
+        cardToRemove.remove();
+    }
+});
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(addCard);
